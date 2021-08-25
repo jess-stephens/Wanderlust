900f1aec74d7d1da13e2878099bce686139b463b
Jessica Stephens <64651019+jess-stephens@users.noreply.github.com>
2021-08-25 13:17:11 -0400

    20210825 cleaning up code based on BK review line 390+ facets by names (key terms) need review

--- Scripts/00_Setup.R
+++ Scripts/00_Setup.R
@@ -5,19 +5,24 @@ library(glitr)
 library(tidyverse)
 library(extrafont)
 library(glitr)
-library(ggplot2)
 library(readxl)
 library(here)
-library(ICPIutilities)
 library(janitor)
-library(dplyr)
+
+
+#need to replace with GOPHR
+library(ICPIutilities)
+
+#these come with tidyverse
+# library(dplyr)
+# library(ggplot2)
 
 # install.packages("tidytext")
 library(tidytext)
 # install.packages("scales")
 library(scales) 
 
-library(grid)
+# library(grid)
 # devtools::install_github("clauswilke/ggtext")
 library(ggtext)
 
@@ -27,10 +32,12 @@ library(ggtext)
 # install.packages("RColorBrewer")
 # install.packages("vroom")
 
-library(ggridges)
+# library(ggridges)
 library(glue)
 library(patchwork)
 library(RColorBrewer)
 library(vroom)
+# install.packages("Rcpp")
+# library(Rcpp)
 
 
