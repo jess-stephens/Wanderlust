50880be68b2bde8dc481d42d33ceb26a14ea51d6
Jessica Stephens <64651019+jess-stephens@users.noreply.github.com>
2021-12-04 11:47:12 -0500

    Fy21Q4 visuals created dedup context vis changed color in ou facets to covid=red alone

--- Scripts/02_VL_Narratives.R
+++ Scripts/02_VL_Narratives.R
@@ -21,19 +21,19 @@ source("Scripts/00_Setup.R")
 setwd("C:/Users/jStephens/Documents/ICPI/Narrative Analysis")
 
 #read in excel data set of narratives (these narratives have been read and assigned sentiments)
-df <- read_xlsx("Data/NarrativeAnalysis_FY21Q3_TX_CURR_1Sep2021.xlsx", sheet="TX_CURR",
+df <- read_xlsx("Data/NarrativeAnalysis_FY21Q4_TX_CURR_30Nov2021_NM.xlsx", sheet="Sheet1",
                col_types = "text") %>% 
   janitor::clean_names()
 
   glimpse(df)
   names(df)
 
-df_PVLS <-read_xlsx("Data/NarrativeAnalysis_FY21Q3_TX_PVLS_1Sep2021.xlsx", sheet="TX_PVLS",
+df_PVLS <-read_xlsx("Data/NarrativeAnalysis_FY21Q4_TX_PVLS_30Nov2021_ayesha_khalda.xlsx", sheet="Sheet1",
                col_types = "text") %>% 
   janitor::clean_names()
 
-  glimpse(df)
-  names(df)
+  glimpse(df_PVLS)
+  names(df_PVLS)
 
 #combine TX_CURR and TX_PVLS narratives into 1 dataset
 df2<-bind_rows(df, df_PVLS) %>% 
@@ -82,7 +82,7 @@ df_long_cat <- df_wider %>%
 
 ####################################################################
 # Visuals require different level of aggregation
-# Data munged/aggregated belwo according to analytic questions
+# Data munged/aggregated below according to analytic questions
 # Aggregation started at lowest (name aka key term) to highest (OU)
 ####################################################################
 
@@ -90,6 +90,8 @@ df_long_cat <- df_wider %>%
 # Aggregate by name (key term)
 # EX. Display TX_PVLS & TX_CURR narratives by key terms
 ##########################################################
+
+
 
 #Collapse to 1 row by indicator, operating unit, partner & name 
 agg_name_long<-df_long_cat %>% 
@@ -135,19 +137,35 @@ agg_partner_un_collapse<-agg_partner_un %>%
 #   & by number of partners within OU
 ##########################################################
 
-#total number of occurrences per OU
-agg_ou_cat<-df_long_cat %>% 
-  select(indicator, operating_unit, partner, category, val) %>% 
-  group_by(indicator,operating_unit, category ) %>% 
+#Collapse to 1 row by indicator, operating unit, & sentence
+agg_sentence_long<-df_long_cat %>% 
+  select(indicator, operating_unit, sentences, val) %>% 
+  group_by(indicator,operating_unit, sentences ) %>% 
   summarize_at(vars(val), sum, na.rm=TRUE) %>% 
-  ungroup()
-  
-#unique partner mentions per OU
-agg_ou_partner_un<-agg_partner_un %>% 
-  select(indicator, operating_unit, category, val) %>% 
-  group_by(indicator,operating_unit, category ) %>% 
+  ungroup() 
+agg_sentence_un<-agg_sentence_long %>% 
+  mutate(val=case_when(
+    val>=0~1, TRUE~val))
+agg_sentence_un_count<-agg_sentence_un %>% 
+  select(indicator, operating_unit, val) %>% 
+  group_by(indicator,operating_unit ) %>% 
   summarize_at(vars(val), sum, na.rm=TRUE) %>% 
-  ungroup()
+  ungroup() 
+
+#this double counts each time it is used in each different key term
+# #total number of occurrences per OU
+# agg_ou_cat<-df_long_cat %>% 
+#   select(indicator, operating_unit, partner, category, val) %>% 
+#   group_by(indicator,operating_unit, category ) %>% 
+#   summarize_at(vars(val), sum, na.rm=TRUE) %>% 
+#   ungroup()
+#   
+# #unique partner mentions per OU
+# agg_ou_partner_un<-agg_partner_un %>% 
+#   select(indicator, operating_unit, category, val) %>% 
+#   group_by(indicator,operating_unit, category ) %>% 
+#   summarize_at(vars(val), sum, na.rm=TRUE) %>% 
+#   ungroup()
 
 #used in visuals: OU_c1/OU_c2/OU_1/OU_2
   
@@ -159,6 +177,8 @@ agg_ou_partner_un<-agg_partner_un %>%
 # Set up Colors 
 ##########################################
 # TX_PVLS colors
+si_rampr("siei") %>% show_col()
+
 si_rampr("moody_blues") %>% show_col()
 # 2F2E6F - DARK PURPLE
 # CFC3FF - light purple
@@ -176,10 +196,9 @@ si_rampr("burnt_siennas") %>% show_col()
 ######################################
 
 #context visuals
-#which OUs are submitting the most narratives using these key terms (total)
-
-OU_c1<-
-  agg_ou_cat %>%
+#which OUs are submitting the most narratives using these key terms (deduplicated)
+OU_c1_dd<-
+  agg_sentence_un_count %>%
   filter(indicator %in% c("TX_PVLS")) %>% 
   ggplot(aes(y=fct_reorder(operating_unit, val)))+
   geom_col(aes( x=val), fill="#7069B2", alpha=.8)+ 
@@ -188,8 +207,8 @@ OU_c1<-
   labs(x = NULL, y = NULL) +
   theme(legend.position = "none")
 
-OU_c2<-
-  agg_ou_cat %>%
+OU_c2_dd<-
+  agg_sentence_un_count %>%
   filter(indicator %in% c("TX_CURR")) %>% 
   ggplot(aes(y=fct_reorder(operating_unit, val)))+
   geom_col(aes( x=val), fill="#BF5A39", alpha=.8)+ 
@@ -199,12 +218,44 @@ OU_c2<-
   theme(legend.position = "none")
 
 
+(OU_c1_dd + OU_c2_dd) + plot_layout(widths  = c(1, 1))+
+  plot_annotation(
+    title = "FY21 Q4: TX_PVLS AND TX_CURR NARRATIVES WITH KEY TERMS BY OU (DEDUPLICATED)",
+    subtitle = "OUs submitting the most narratives capturing the key terms by indicator are Kenya (for both <b style='color:#7069B2'>TX_PVLS</b> and <b style='color:#BF5A39'>TX_CURR</b>)",
+    caption = "Source: FY21 Q4 Narratives",
+    theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown())) 
+
+
+
+#which OUs are submitting the most narratives using these key terms (total)
+
+OU_c1<-
+  agg_ou_cat %>%
+  filter(indicator %in% c("TX_PVLS")) %>%
+  ggplot(aes(y=fct_reorder(operating_unit, val)))+
+  geom_col(aes( x=val), fill="#7069B2", alpha=.8)+
+  si_style_xgrid()+
+  facet_wrap(~indicator)+
+  labs(x = NULL, y = NULL) +
+  theme(legend.position = "none")
+
+OU_c2<-
+  agg_ou_cat %>%
+  filter(indicator %in% c("TX_CURR")) %>%
+  ggplot(aes(y=fct_reorder(operating_unit, val)))+
+  geom_col(aes( x=val), fill="#BF5A39", alpha=.8)+
+  si_style_xgrid()+
+  facet_wrap(~indicator)+
+  labs(x = NULL, y = NULL)+
+  theme(legend.position = "none")
+
+
 (OU_c1 + OU_c2) + plot_layout(widths  = c(1, 1))+
   plot_annotation(
-    title = "FY21 Q3: TX_PVLS AND TX_CURR NARRATIVES REVIEWED FOR KEY TERMS BY OU",
-    subtitle = "OUs submitting the most narratives capturing the key terms by indicator are Asia Region (<b style='color:#7069B2'>TX_PVLS</b>) and South Africa (<b style='color:#BF5A39'>TX_CURR</b>)",
-    caption = "Source: FY21 Q3 Narratives",
-    theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown())) 
+    title = "FY21 Q4: TX_PVLS AND TX_CURR NARRATIVES REVIEWED FOR KEY TERMS BY OU (TOTAL OCCURRENCES)",
+    subtitle = "OUs submitting the most narratives capturing the key terms by indicator are Kenya (<b style='color:#7069B2'>TX_PVLS</b>) and Asia Region (<b style='color:#BF5A39'>TX_CURR</b>)",
+    caption = "Source: FY21 Q4 Narratives",
+    theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown()))
 
 
 
@@ -242,9 +293,9 @@ OU_2<-
 
 (OU_1 + OU_2) + plot_layout(widths  = c(1, 1))+
   plot_annotation(
-    title = "FY21 Q3: TX_PVLS AND TX_CURR NARRATIVES REVIEWED FOR KEY TERMS BY OU, ISSUE OR NO ISSUE",
-   subtitle = "Asia Region described most <b style='color:#2F2E6F'>TX_PVLS issues</b> , while South Africa described the most <b style='color:#923417'>TX_CURR issues</b>",
-     caption = "Source: FY21 Q3 Narratives",
+    title = "FY21 Q4: TX_PVLS AND TX_CURR NARRATIVES REVIEWED FOR KEY TERMS BY OU, ISSUE OR NO ISSUE",
+   subtitle = "Kenya described most <b style='color:#2F2E6F'>TX_PVLS issues</b>, while South Africa described the most <b style='color:#923417'>TX_CURR issues</b>",
+     caption = "Source: FY21 Q4 Narratives",
      theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown())) 
 
 # following not included in final visuals, but may be of interest to different stakeholders
@@ -320,9 +371,9 @@ y2<-
 (y1 + y2) + 
   plot_layout(widths  = c(1, 1)) +
   plot_annotation(
-    title = "FY21 Q3: TX_PVLS AND TX_CURR NARRATIVES REVIEWED FOR KEY TERMS BY PARTNERS WITHIN OU",
+    title = "FY21 Q4: TX_PVLS AND TX_CURR NARRATIVES REVIEWED FOR KEY TERMS BY PARTNERS WITHIN OU",
     subtitle = "The largest number of partners in Kenya described both <b style='color:#2F2E6F'>TX_PVLS </b> and <b style='color:#923417'>TX_CURR</b> issues",
-    caption = "Source: FY21 Q3 Narratives",
+    caption = "Source: FY21 Q4 Narratives",
     theme = theme(plot.title = element_markdown(),
                   plot.subtitle = element_markdown())) 
 
@@ -340,8 +391,8 @@ z1<-agg_name_un %>%
     filter(name=="general_impact_on_vl"| name=="routine_vl_testing" | name=="tx_pvls_covid"|name=="reagent_stockout"|
              name=="results_returned_turn_around_time"|name=="equipment"|name=="facility_headcount"|
              name=="data_reporting"|name=="backlogs"|name=="sample_collection"|name=="sample_transport_testing"|
-             name=="staffing"|name=="resources_reallocation"|name=="mmd"|name=="arv_stockout") %>% 
-  # filter(val>0) %>% 
+             name=="staffing"|name=="resources_reallocation"|name=="mmd"|name=="arv_stockout") %>%
+  # filter(val>0) %>%
   mutate(name = str_replace_all(name, "_"," ")) %>% 
   #str_to_sentence function in stringr
   #case when to replace tx pvls covid -> TX_PVLS COVID
@@ -363,12 +414,13 @@ si_style_xgrid()+
   labs(x = NULL, y = NULL)+
   theme(legend.position = "none")
 
+
 z2<-agg_name_un %>%
   filter(category=="issue" & indicator=="TX_CURR" ) %>% 
-    filter(name=="covid"|name=="mmd"|name=="arv_stockout"|name=="data_reporting"|name=="sample_collection"|
+    filter(name=="covid"|name=="mmd"|name=="arv_stockout"|name=="data_reporting"|name=="results_returned_turn_around_time"|
              name=="general_impact_on_treatment"|name=="general_impact_on_vl"|name=="total_tx_curr_mech"|
-             name=="staffing"|name=="routine_vl_testing"|name=="facility_headcount"| name=="equipment") %>% 
-    # filter(val>0) %>% 
+             name=="staffing"|name=="routine_vl_testing"|name=="facility_headcount"| name=="equipment") %>%
+    # filter(val>0) %>%
   mutate(name = str_replace_all(name, "_"," "),
          name=str_to_sentence(name), 
          name=case_when(
@@ -378,6 +430,7 @@ z2<-agg_name_un %>%
            name=="Arv stockout"~"ARV stockout", 
            name=="General impact on vl"~"General impact on VL",   
            name=="Routine vl testing"~"Routine VL testing",
+           name=="Results returned turn around time"~"Results Returned/TAT",
            TRUE~name)) %>% 
   ggplot(aes(y=reorder(name, val), x=val)) +
   geom_col(fill="#923417",  alpha=.8)+ 
@@ -389,9 +442,9 @@ z2<-agg_name_un %>%
 
 (z1+ z2) + plot_layout(widths  = c(1, 1))+
   plot_annotation(
-    title = "FY21 Q3: TX_PVLS AND TX_CURR NARRATIVES REVIEWED BY KEY TERMS WITHIN PARTNERS",
-    subtitle = "The largest number of partners described issues causing general impacts, followed by routine VL testing for <b style='color:#2F2E6F'>TX_PVLS</b> and COVID-19 for <b style='color:#923417'>TX_CURR</b>",
-    caption = "Source: FY21 Q3 Narratives",
+    title = "FY21 Q4: TX_PVLS AND TX_CURR NARRATIVES REVIEWED BY KEY TERMS WITHIN PARTNERS",
+    subtitle = "The largest number of partners described issues in routine VL testing and reagent stockout for <b style='color:#2F2E6F'>TX_PVLS</b> and COVID-19 for <b style='color:#923417'>TX_CURR</b>",
+    caption = "Source: FY21 Q4 Narratives",
     theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown()))
 #, when summarizing narratives by partners rather than by OU
 
@@ -420,9 +473,9 @@ agg_name_un %>%
   ungroup() %>% 
   filter(category == "issue", 
          indicator == "TX_PVLS", 
-          name=="general_impact_on_vl"| name=="routine_vl_testing" | name=="tx_pvls_covid"|name=="reagent_stockout"|
-           name=="data_reporting"|name=="results_returned_turn_around_time",
-           # |name=="sample_collection"|name=="equipment"|name=="backlogs"||name=="sample_transport_testing"|name=="facility_headcount"|
+          name=="general_impact_on_vl"|name=="reagent_stockout"| name=="routine_vl_testing" 
+         | name=="tx_pvls_covid"|name=="results_returned_turn_around_time"|name=="sample_collection",
+           # | name=="data_reporting"|name=="equipment"|name=="backlogs"||name=="sample_transport_testing"|name=="facility_headcount"|
            # name=="staffing"|name=="resources_reallocation"|name=="mmd"|name=="arv_stockout",
           val > 0) %>% 
   mutate( name = str_replace_all(name, "_", " "),
@@ -451,9 +504,9 @@ agg_name_un %>%
         strip.placement = "outside",
         legend.position = "none")+
   plot_annotation(
-    title = "FY21 Q3: NUMBER OF PARTNERS REPORTING ISSUES IN TX_PVLS NARRATIVES BY KEY TERMS AND OU",
+    title = "FY21 Q4: NUMBER OF PARTNERS REPORTING ISSUES IN TX_PVLS NARRATIVES BY KEY TERMS AND OU",
      subtitle = "Top 6 Issues for TX_PVLS by OU",
-    caption = "Source: FY21 Q3 Narratives",
+    caption = "Source: FY21 Q4 Narratives",
     theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown()))
 
 
@@ -474,8 +527,8 @@ agg_name_un %>%
   filter(category == "issue", 
          indicator == "TX_CURR",
          name=="general_impact_on_treatment"|name=="covid"|name=="mmd"|name=="arv_stockout"|
-           name=="data_reporting"|name=="staffing" ,
-           # |name=="sample_collection"|name=="routine_vl_testing" |
+           name=="data_reporting"| name=="results_returned_turn_around_time",
+           #name=="staffing"| |name=="sample_collection"|name=="routine_vl_testing" |
            # name=="general_impact_on_vl"|name=="total_tx_curr_mech"|
            # name=="facility_headcount"| name=="equipment"
          val > 0) %>% 
@@ -488,6 +541,7 @@ agg_name_un %>%
            name=="Arv stockout"~"ARV stockout", 
            name=="General impact on vl"~"General impact on VL",   
            name=="Routine vl testing"~"Routine VL testing",
+           name=="Results returned turn around time"~"Results Returned/TAT",
            TRUE~name)) %>% 
   ggplot(aes(y=reorder_within(operating_unit, val, name), x=val, fill=name)) +
   geom_col(width=.8) +
@@ -503,9 +557,9 @@ agg_name_un %>%
         strip.placement = "outside",
         legend.position = "none")+
   plot_annotation(
-    title = "FY21 Q3: NUMBER OF PARTNERS REPORTING ISSUES IN TX_CURR NARRATIVES BY KEY TERMS AND OU",
+    title = "FY21 Q4: NUMBER OF PARTNERS REPORTING ISSUES IN TX_CURR NARRATIVES BY KEY TERMS AND OU",
     subtitle = "Top 6 Issues for TX_CURR by OU",
-    caption = "Source: FY21 Q3 Narratives",
+    caption = "Source: FY21 Q4 Narratives",
     theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown()))
 
 
@@ -536,11 +590,9 @@ agg_name_un %>%
   filter(operating_unit %in% c("South Africa", "Mozambique", "Tanzania", "Nigeria", "Kenya", "Uganda"),
     category == "issue", 
          indicator == "TX_PVLS", 
-         name=="general_impact_on_vl"| name=="data_reporting" |name=="routine_vl_testing" |name=="reagent_stockout"|
-            name=="tx_pvls_covid"|name=="results_returned_turn_around_time"|name=="equipment"|name=="sample_collection",
-         #name=="sample_transport_testing"|name=="facility_headcount"|name=="backlogs"|
-          # name=="staffing"|name=="resources_reallocation"|name=="mmd"|name=="arv_stockout",
-         val > 0) %>% 
+        #     name=="general_impact_on_vl"|name=="reagent_stockout"| name=="routine_vl_testing" 
+        # | name=="tx_pvls_covid"|name=="results_returned_turn_around_time"|name=="sample_collection",
+        val > 0) %>% 
   mutate( name = str_replace_all(name, "_", " "),
           name=str_to_sentence(name),
           name=case_when(
@@ -556,7 +608,8 @@ agg_name_un %>%
   geom_col(width=.8) +
   scale_x_continuous(position = "top") +
   scale_y_reordered() +
-  scale_fill_si(palette = "siei", discrete = T) +
+  scale_fill_manual(values = c( '#808080','#808080','#c43d4d','#808080','#808080','#808080','#808080','#808080','#808080','#808080','#808080','#808080','#808080'))+ 
+  # scale_fill_si(palette = "siei", discrete = T) +
   si_style_xgrid() +
   facet_wrap(~operating_unit, scales = "free_y")+
   labs(x = NULL, y = NULL)+
@@ -566,9 +619,9 @@ agg_name_un %>%
         strip.placement = "outside",
         legend.position = "none")+
   plot_annotation(
-    title = "FY21 Q3: NUMBER OF PARTNERS REPORTING ISSUES IN TX_PVLS NARRATIVES BY OU AND KEY TERMS",
-    subtitle = "Partners reported <b style='color:#2057A7'>COVID-19</b> issues for TX_PVLS in each OU except Tanzania",
-    caption = "Source: FY21 Q3 Narratives 
+    title = "FY21 Q4: NUMBER OF PARTNERS REPORTING ISSUES IN TX_PVLS NARRATIVES BY OU AND KEY TERMS",
+    subtitle = "Partners reported <b style='color:#c43d4d'>COVID-19</b> issues for TX_PVLS remained key issue in Uganda alone ",
+    caption = "Source: FY21 Q4 Narratives 
               Top 6 OUs by FY21 TX_PVLS (D) Targets for TX_PVLS by OU 
               6th OU = Zimbabwe (no narratives reported), replaced by 7th OU = Uganda",
     theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown()))
@@ -591,12 +644,9 @@ agg_name_un %>%
   filter(operating_unit %in% c("South Africa", "Mozambique", "Tanzania", "Nigeria", "Kenya", "Uganda"),
     category == "issue", 
          indicator == "TX_CURR",
-         name=="general_impact_on_treatment"|name=="covid"|name=="mmd"|name=="arv_stockout"|
-           name=="data_reporting"|name=="routine_vl_testing" |
-          # name=="staffing"     |name=="sample_collection"|
-          name=="general_impact_on_vl"|name=="total_tx_curr_mech"|
-          name=="facility_headcount"| name=="equipment",
-         val > 0) %>% 
+    # name=="covid"|name=="general_impact_on_treatment"|name=="mmd"|name=="arv_stockout"|
+    #   name=="data_reporting"| name=="results_returned_turn_around_time",
+    val > 0) %>% 
   mutate(name = str_replace_all(name, "_"," "),
          name=str_to_sentence(name), 
          name=case_when(
@@ -606,12 +656,15 @@ agg_name_un %>%
            name=="Arv stockout"~"ARV stockout", 
            name=="General impact on vl"~"General impact on VL",   
            name=="Routine vl testing"~"Routine VL testing",
+           name=="Results returned turn around time"~"Results Returned/TAT",
            TRUE~name)) %>% 
   ggplot(aes(y=reorder_within(name, val, operating_unit), x=val, fill=name)) +
   geom_col(width=.8) +
   scale_x_continuous(position = "top") +
   scale_y_reordered() +
-  scale_fill_si(palette = "siei", discrete = T) +
+  #color specifically covid only
+  scale_fill_manual(values = c('#808080','#c43d4d', '#808080','#808080','#808080','#808080','#808080','#808080'))+ 
+   # scale_fill_si(palette = "siei", discrete = T) +
   si_style_xgrid() +
   facet_wrap(~operating_unit, scales = "free_y")+
   labs(x = NULL, y = NULL)+
@@ -622,9 +675,9 @@ agg_name_un %>%
         strip.placement = "outside",
         legend.position = "none")+
   plot_annotation(
-    title = "FY21 Q3: NUMBER OF PARTNERS REPORTING ISSUES IN TX_CURR NARRATIVES BY KEY TERMS AND OU",
-    subtitle = "Partners reported <b style='color:#C43D4D'>COVID-19</b> issues for TX_CURR in each OU except Tanzania",
-    caption = "Source: FY21 Q3 Narratives 
+    title = "FY21 Q4: NUMBER OF PARTNERS REPORTING ISSUES IN TX_CURR NARRATIVES BY KEY TERMS IN LARGEST TX_PVLS OUS",
+    subtitle = "Partners reported <b style='color:#C43D4D'>COVID-19</b> issues for TX_CURR in only half of OUs with largest programs",
+    caption = "Source: FY21 Q4 Narratives 
     Top 6 OUs by FY21 TX_PVLS (D) Targets for TX_PVLS by OU
     6th OU = Zimbabwe (no narratives reported), replaced by 7th OU = Uganda",
     theme = theme(plot.title = element_markdown(), plot.subtitle = element_markdown()))
